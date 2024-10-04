9c51e16fca051f4ee1680adcec023ac0dd06a50d
David Kwok <davidkwok@ufinity.com>
2024-10-05 03:27:18 +0800

    added docker commands to create sfss-network if required

--- start-sfss-server.sh
+++ start-sfss-server.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+docker network ls | grep sfss-network || docker network create -d bridge sfss-network
 docker run --rm -it --network=sfss-network --name sfss-server sfss-server
